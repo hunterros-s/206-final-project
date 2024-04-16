016db27bb03ea1082c0c20ea30ae3ece6d4d8b13
hlross <hlross@umich.edu>
2024-04-16 11:16:06 -0400

    work

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from weather import get_icao_code
 
+
 def main():
-    success, data = get_icao_code("San Francisco")
+    success, data = get_icao_code("70112")
     print(data)
 
 if __name__ == "__main__":
