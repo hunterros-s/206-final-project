6f627381787fa2ebe83bdec03cc6797102b06ca4
hlross <hlross@umich.edu>
2024-04-17 11:07:47 -0400

    functionalize sports.py

--- sports.py
+++ sports.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 
 #looking for: location, date/time, final score
@@ -7,15 +8,20 @@ from bs4 import BeautifulSoup
 #url to dashboard of all the games from a particular week
 
 
-year = 2022
-week_num = 4
+# year = 2022
+# week_num = 4
 
-summary_page_url = f'https://www.pro-football-reference.com/years/{year}/week_{week_num}.htm'
-r = requests.get(summary_page_url)
+def get_game_links(year, week_num):
+    summary_page_url = f'https://www.pro-football-reference.com/years/{year}/week_{week_num}.htm'
 
-#gets list of links to box scores of every game listed on the summary page
-if (r.status_code == 200):
-    soup = BeautifulSoup(r.content, 'html.parser')
+    try:
+        output = requests.get(summary_page_url)
+        output.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return False, None
+
+    soup = BeautifulSoup(output.content, 'html.parser')
 
     td_tags = soup.findAll('td', class_='right gamelink')
 
@@ -28,69 +34,82 @@ if (r.status_code == 200):
             full_url = f'https://www.pro-football-reference.com{href_link}'
             boxscore_links.append(full_url)
 
-    #print(boxscore_links)
+    return True, boxscore_links
 
-else:
-    print("Failed to retrieve the webpage")
+def get_stadium_info(stadium_link):
+    #using stadium link to get zip code of stadium
+    try:
+        output = requests.get(stadium_link)
+        output.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return False, None
 
+    soup = BeautifulSoup(output.content, 'html.parser')
+    meta_div = soup.find('div', id='meta')
+    address_p = meta_div.find('p')
+    address_text = address_p.get_text()
 
-#indexing through all links to individual boxscores
-#end up with a list of lists, each individual list containing: away score, home score, date, start time, stadium address
-
-#big_dict = []
-
-
-for boxscore in boxscore_links:
-    r = requests.get(boxscore)
-    if (r.status_code == 200):
-        #game_info = []
-        soup = BeautifulSoup(r.content, 'html.parser')
-
-        #team scores
-        score_divs = soup.findAll('div', class_='score')
-        scores = [div.get_text() for div in score_divs]
-
-        #date
-        date = soup.find('div', class_='scorebox_meta')
-        date_text = date.div.text
-
-        #start time
-        #final slice is to take out ": " before the time
-        start_time_strong_tag = soup.find('strong', text="Start Time")
-        start_time_text = start_time_strong_tag.next_sibling.strip('" ')[2:]
-
-        #stadium
-        #do we want the stadium name or the link to the page of the stadium which has the address
-        stadium_name = soup.find('strong', text='Stadium')
-        stadium_href = stadium_name.next_sibling.next_sibling.get('href')
-        stadium_link = f'https://www.pro-football-reference.com{stadium_href}'
-
-        #using stadium link to get zip code of stadium
-        r = requests.get(stadium_link)
-        if (r.status_code == 200):
-            soup = BeautifulSoup(r.content, 'html.parser')
-            meta_div = soup.find('div', id='meta')
-            address_p = meta_div.find('p')
-            zipcode = address_p.get_text()[-5:]
-
-            #print(address_text)
-
-        else:
-            print("Failed to retrieve the webpage")
+    return True, address_text
 
 
-        game_info = [scores[0], scores[1], date_text, start_time_text, zipcode]
+def get_box_score(boxscore_link):
+    try:
+        output = requests.get(boxscore_link)
+        output.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return False, None
+    
+    soup = BeautifulSoup(output.content, 'html.parser')
 
-        print(game_info)
+    #team scores
+    score_divs = soup.findAll('div', class_='score')
+    scores = [div.get_text() for div in score_divs]
 
-    else:
-        print("Failed to retrieve the webpage")
+    #date
+    date = soup.find('div', class_='scorebox_meta')
+    date_text = date.div.text
+
+    #start time
+    #final slice is to take out ": " before the time
+    start_time_strong_tag = soup.find('strong', text="Start Time")
+    start_time_text = start_time_strong_tag.next_sibling.strip('" ')[2:]
+
+    #stadium
+    #do we want the stadium name or the link to the page of the stadium which has the address
+    stadium_name = soup.find('strong', text='Stadium')
+    stadium_href = stadium_name.next_sibling.next_sibling.get('href')
+    stadium_link = f'https://www.pro-football-reference.com{stadium_href}'
+
+    return True, (scores[0], scores[1], date_text, start_time_text, stadium_link)
+
+def get_all_boxscores(boxscore_links):
+    data = []
+    for boxscore_link in boxscore_links:
+        time.sleep(.2)
+        success, boxscore_info = get_box_score(boxscore_link)
+        if not success:
+            continue
+
+        score_a, score_b, date_text, time_text, stadium_link = boxscore_info
+
+        time.sleep(.2)
+        success, address = get_stadium_info(stadium_link)
+        if not success:
+            continue
+        
+        data.append(
+            (score_a, score_b, date_text, time_text, address)
+        )
+    
+    return data
 
 
-
-
-
-
+success, links = get_game_links(2022, 4)
+if not success:
+    exit(1)
+print(get_all_boxscores(links))
 
 
 
