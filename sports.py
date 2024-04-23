4a21913732a229f95c9d016627292102b6a1f634
NathanKushnir <nkushnir@umich.edu>
2024-04-23 13:46:09 -0400

    4/23 update

--- sports.py
+++ sports.py
@@ -12,6 +12,8 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
 }
 
+'''
+
 year = 2022
 week_num = 4
 
@@ -39,65 +41,122 @@ else:
     print(f"Failed to retrieve the webpage. Status code: {r.status_code} - {r.reason}")
     sys.exit(1)
 
+'''
+
 
 
 #indexing through all links to individual boxscores
 #end up with a list of lists, each individual list containing: away score, home score, date, start time, stadium address
 
 
-for boxscore in boxscore_links:
-    r = requests.get(boxscore, headers=headers)
+#for boxscore in boxscore_links:
+url = 'https://www.pro-football-reference.com/boxscores/202210020atl.htm'
+r = requests.get(url, headers=headers)
+    #r = requests.get(boxscore, headers=headers)
+time.sleep(1)
+if r.status_code == 200:
+    #game_info = []
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    '''
+    #team scores
+    score_divs = soup.findAll('div', class_='score')
+    scores = [div.get_text() for div in score_divs]
+
+    #date
+    date = soup.find('div', class_='scorebox_meta')
+    date_text = date.div.text
+
+    #start time
+    #final slice is to take out ": " before the time
+    start_time_strong_tag = soup.find('strong', string="Start Time")
+    start_time_text = start_time_strong_tag.next_sibling.strip('" ')[2:]
+
+    #stadium
+    #do we want the stadium name or the link to the page of the stadium which has the address
+    stadium_name = soup.find('strong', string='Stadium')
+    stadium_href = stadium_name.next_sibling.next_sibling.get('href')
+    stadium_link = f'https://www.pro-football-reference.com{stadium_href}'
+    
+    #using stadium link to get address of stadium
+    r = requests.get(stadium_link, headers=headers)
     time.sleep(1)
     if (r.status_code == 200):
-        #game_info = []
         soup = BeautifulSoup(r.content, 'html.parser')
+        meta_div = soup.find('div', id='meta')
+        address_p = meta_div.find('p')
+        address_text = address_p.get_text()
+        #zipcode = address_p.get_text()[-5:]
 
-        #team scores
-        score_divs = soup.findAll('div', class_='score')
-        scores = [div.get_text() for div in score_divs]
-
-        #date
-        date = soup.find('div', class_='scorebox_meta')
-        date_text = date.div.text
-
-        #start time
-        #final slice is to take out ": " before the time
-        start_time_strong_tag = soup.find('strong', string="Start Time")
-        start_time_text = start_time_strong_tag.next_sibling.strip('" ')[2:]
-
-        #stadium
-        #do we want the stadium name or the link to the page of the stadium which has the address
-        stadium_name = soup.find('strong', string='Stadium')
-        stadium_href = stadium_name.next_sibling.next_sibling.get('href')
-        stadium_link = f'https://www.pro-football-reference.com{stadium_href}'
-
-        #using stadium link to get zip code of stadium
-        r = requests.get(stadium_link, headers=headers)
-        time.sleep(1)
-        if (r.status_code == 200):
-            soup = BeautifulSoup(r.content, 'html.parser')
-            meta_div = soup.find('div', id='meta')
-            address_p = meta_div.find('p')
-            #zipcode = address_p.get_text()[-5:]
-
-            #print(address_text)
-
-        else:
-            address_p = 'Failed to find'
-
-
-        game_info = [scores[0], scores[1], date_text, start_time_text, address_p]
-
-        print(game_info)
+        #print(address_text)
 
     else:
-        print("Failed to retrieve the webpage")
+        address_p = 'Failed to find'
+
+    '''
+
+    #kicking stats
+    #first kicker
+    
+    
+    kicking_table = soup.find("table", id='kicking')
+
+    print(kicking_table)
+    table_div = kicking_table.find('table', class_='sortable stats_table now_sortable', id='kicking')
+    print(table_div)
+    rows = table_div.find_all('tr')
+    #table = table_div.find('table', class_='sortable stats_table now_sortable', id='kicking')
+
+    if rows:
+        print('success')
+    else:
+        print('fail')
+    #row = kicking_table.find_all('tr')
+    #print (row)
+
+    '''
+    row = kicking_table.find('tr', {'data-row':'0'})
+    if row:
+        first_fga = row.find('td', {'data-stat':'fga'})
+        first_fgm = row.find('td', {'data-stat':'fgm'})
+        fga_text = first_fga.get_text() if first_fga else None
+        fgm_test = first_fgm.get_text() if first_fgm else None
+    else:
+        print("Did not find the first kicker row")
+    
+
+    
+    #second kicker
+    second_kicker_stats = soup.find('tr', 'data-row'=='4')
+    if second_kicker_stats:
+        second_fga = second_kicker_stats.find({'data-stat':'xpa'})
+        second_fgm = second_kicker_stats.find({'data-stat':'xpm'})
+    else:
+        print("Did not find the second kicker tr element")
+    '''
+
+    
+
+    #fg_list = [fga_text, fgm_test] #, second_fga, second_fgm]
+
+    #print(fg_list)
+
+
+    #game_info = [scores[0], scores[1], date_text, start_time_text, address_text]
+
+    #print(game_info)
+
+else:
+    print("Failed to retrieve the webpage")
+    
 
 
 
 
 # year = 2022
 # week_num = 4
+
+
 
 def get_game_links(year, week_num):
     summary_page_url = f'https://www.pro-football-reference.com/years/{year}/week_{week_num}.htm'
@@ -150,6 +209,8 @@ def get_game_links(year, week_num):
 
     return True, data
 
+
+
 def get_stadium_info(stadium_link):
     #using stadium link to get zip code of stadium
     try:
@@ -201,6 +262,7 @@ def get_box_score(boxscore_link):
 
     return True, (scores[0], scores[1], date_text, start_time_text, stadium_link)
 
+
 def get_all_boxscores(boxscore_summaries):
     data = []
     for winner_text, winner_score, loser_text, loser_score, full_url in boxscore_summaries:
@@ -225,7 +287,7 @@ def get_all_boxscores(boxscore_summaries):
     return data
 
 
-success, data = get_game_links(2022, 4)
+#success, data = get_game_links(2022, 4)
 #if not success:
 #    exit(1)
 #print(get_all_boxscores(data))
