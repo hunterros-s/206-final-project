3de65364d65628045c82392f4a9e62cbe90d30be
hlross <hlross@umich.edu>
2024-04-26 17:42:58 -0400

    push vis

--- visualizations.py
+++ visualizations.py
@@ -1,4 +1,4 @@
-# temperature vs attendance (x axis temp, y axis average attendance)
+# temperature vs attendance (line chart, x axis temp, y axis average attendance)
 # condition vs attendance (bar chart, x axis condition, y axis average attendance)
-# wspd vs kicking (attempts, long, pct)
+# wspd vs kicking (attempts, long, pct) -- (dot plot, x: wspd, y: longest fg)
 # passing attempts vs condition (side by side box and whisker plot, x axis: condition, y axis: attempts)
